d558e338236b9553c1465fee8eed87dac8f0658d
glass-waves <dylan.whitej@gmail.com>
2019-01-16 15:13:09 -0800

    new shapes

--- sketch.js
+++ sketch.js
@@ -1,9 +1,39 @@
+
+
+
 function setup() {
-	
-	createCanvas(500,500);
+
+	createCanvas(windowWidth,windowHeight);
+
 
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+
+var jitter = random(0, 3)	
+
+
+background(255);
+var degrees = map(mouseX, 0, windowWidth, 0, PI);
+
+window.onresize = function(){createCanvas(windowWidth, windowHeight)};
+var xsize = windowWidth;
+var ysize= windowHeight;
+fill(0);
+
+beginShape();
+
+vertex(jitter+40, ysize);
+vertex(xsize,ysize);
+vertex(xsize,jitter);
+
+endShape();
+
+push();
+angleMode(DEGREES);
+translate((xsize/2)-50, (ysize/2)-35,);
+rotate(360-((frameCount*7)%720));
+rectMode(CENTER);
+rect(0, 0, 100, 100 );
+pop();
 }
