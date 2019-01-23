dd07ebb607e3c9275f1ec8984e24efcb854b913d
glass-waves <dylan.whitej@gmail.com>
2019-01-23 00:45:34 -0800

    ball on slant with waves

--- sketch.js
+++ sketch.js
@@ -9,11 +9,14 @@ function setup() {
 }
 
 function draw() {
-
+var n;
 var jitter = random(0, 3)	
-
+n=n+1;
+print(n);
 
 background(255);
+textSize(30);
+text('supper', 100, 100);
 var degrees = map(mouseX, 0, windowWidth, 0, PI);
 
 window.onresize = function(){createCanvas(windowWidth, windowHeight)};
@@ -23,17 +26,20 @@ fill(0);
 
 beginShape();
 
-vertex(jitter+40, ysize);
+vertex(0, ysize);
 vertex(xsize,ysize);
-vertex(xsize,jitter);
+vertex(xsize,0);
 
 endShape();
 
-push();
-angleMode(DEGREES);
-translate((xsize/2)-50, (ysize/2)-35,);
-rotate(360-((frameCount*7)%720));
-rectMode(CENTER);
-rect(0, 0, 100, 100 );
-pop();
+ellipseMode(CENTER);
+ellipse(xsize/2-35, ysize/2-35, 100,100 );
+
+strokeWeight(1);
+noFill();
+
+bezier((xsize/2)+30,ysize/2- 100, (xsize/2+50)+(n/2), (ysize/2-150)+n,(xsize/2+70)-(n/2), (ysize/2-100)-n,xsize/2+100, ysize/2-150 );
+bezier((xsize/2)+40,ysize/2- 90, xsize/2+60, ysize/2-140,xsize/2+80, ysize/2-90,xsize/2+110, ysize/2-140 );
+bezier((xsize/2)+50,ysize/2- 80, xsize/2+70, ysize/2-130,xsize/2+90, ysize/2-80,xsize/2+120, ysize/2-130 );
+bezier((xsize/2)+30,ysize/2- 100, xsize/2+50, ysize/2-150,xsize/2+70, ysize/2-100,xsize/2+100, ysize/2-150 );
 }
